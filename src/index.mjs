d848cfbb0c75be4e38bf34a33444063520e30d10
med moemen hedhly <moomenhedhly8@gmail.com>
2026-04-09 12:50:45 +0100

    First commit

---
+++ src/index.mjs
@@ -0,0 +1,50 @@
+import express from 'express';
+
+const app = express();
+
+const port = process.env.port || 3000;
+
+const users = [{ id: 1, username: "moemen", displayname: "Moemen" },
+{ id: 2, username: "uzika", displayname: "Uzika" },
+{ id: 3, username: "birbis", displayname: "Birbis" },
+
+];
+
+app.get("/", (req, res) => {
+    res.status(201).send({ msg: "welocome to home page" });
+
+});
+
+app.get("/api/users", (req, res) => {
+    res.send(users);
+
+});
+
+app.get("/api/products", (req, res) => {
+    res.send({ id: 123, name: "batatis", price: 2 });
+});
+
+app.get("/api/users/:id", (req, res) => { // video 3 dans playlist express;
+    console.log(req.params);
+    let id = parseInt(req.params.id);
+    console.log(id);
+    console.log(typeof id)
+    if (isNaN(id)) { return res.status(400).send({ msg: "bad request / invalid id" }); }
+
+    const fuser = users.find((user) => user.id === id);
+    if (!fuser)
+        return res.sendStatus(404);
+    return res.send(fuser);
+
+
+
+});
+
+
+
+app.listen(port, () => {
+    console.log(`running on port ${port}`);
+});
+
+
+
